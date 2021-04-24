293aad9cd68db1f501d895921ede9973d0ff39f1
unknown <montespierre@gmail.com>
2021-04-23 19:37:36 -0500

    EditText

--- app/src/main/java/com/weimont/fundamentosandoird/MainActivity.java
+++ app/src/main/java/com/weimont/fundamentosandoird/MainActivity.java
@@ -3,12 +3,17 @@ package com.weimont.fundamentosandoird;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        TextView miTexto = new TextView(this);
+        miTexto.setText("Mi primera aplicacion");
+
+        setContentView(miTexto);
     }
 }
