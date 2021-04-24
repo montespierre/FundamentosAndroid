97b60f747764ca2ca08c8320851d216450ad5cbc
unknown <montespierre@gmail.com>
2021-04-23 19:44:39 -0500

    EditText

--- app/src/main/java/com/weimont/fundamentosandoird/MainActivity.java
+++ app/src/main/java/com/weimont/fundamentosandoird/MainActivity.java
@@ -11,9 +11,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        // Context: Es una clase que pide resolucion de preferencia de la aplicaion y de pantalla
+        // This le pasa la propia clase donde nos encontramos
         TextView miTexto = new TextView(this);
+
+        // Establecer el texto
         miTexto.setText("Mi primera aplicacion");
 
+        // Que vista va a vizualizar la actividad
         setContentView(miTexto);
     }
 }
