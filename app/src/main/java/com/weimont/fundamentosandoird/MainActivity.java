ed007dacf61fd06658da4ea1069c3673fd4782f3
unknown <montespierre@gmail.com>
2021-04-27 18:20:00 -0500

    checkBox

--- app/src/main/java/com/weimont/fundamentosandoird/MainActivity.java
+++ app/src/main/java/com/weimont/fundamentosandoird/MainActivity.java
@@ -4,6 +4,8 @@ package com.weimont.fundamentosandoird;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
 
 
@@ -11,11 +13,14 @@ public class MainActivity extends Activity {
 
     public int contador;
 
+    TextView textoResultado;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textoResultado = findViewById(R.id.contadorTexto);
 
         contador = 0;
 
@@ -24,29 +29,47 @@ public class MainActivity extends Activity {
 
     public void incrementaContador(View vista){
         contador++;
-        mostrarResultado();
+        textoResultado.setText(Integer.toString(contador));
+
 
     }
 
     public void restaContador(View vista){
-        contador --;
-        mostrarResultado();
+        contador = contador -1;
+
+        if(contador < 0){
+            CheckBox negativos = findViewById(R.id.negativos);
+            if(! negativos.isChecked()){
+                contador = 0;
+            }
+        }
+
+        textoResultado.setText(Integer.toString(contador));
+
 
     }
 
     public void resetearContador(View vista){
-        contador = 0;
-        mostrarResultado();
+        EditText numero_reset = findViewById(R.id.n_reseteo);
+
+       try {
+           contador = Integer.parseInt(numero_reset.getText().toString());
+        }catch (Exception e){
+           contador = 0;
+
+        }
+
+
+        numero_reset.setText("");
+
+        textoResultado.setText(Integer.toString(contador));
+
 
 
     }
 
-    public void mostrarResultado(){
-        TextView tvContadorPulsaciones;
 
-        // vincual el codigo en java del TV a TV de layout
-        tvContadorPulsaciones = findViewById(R.id.contadorTexto);
-        tvContadorPulsaciones.setText(Integer.toString(contador));
 
-    }
+
+
 }
